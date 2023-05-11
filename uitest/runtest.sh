cd5a5bb0bc0e0d19d91e0870ccc885e56dcb5d90
Zbyněk Konečný <zbynek@geogebra.org>
2023-05-11 11:58:26 +0000

    🛑🏜️📦

--- uitest/runtest.sh
+++ uitest/runtest.sh
@@ -2,4 +2,4 @@ npm install --force || exit 1
 npx http-server -p 8888 content & npx wait-on http://localhost:8888
 npx cypress run --env userAgent="Chrome iPad" --spec="cypress/integration/**/*-iPad.spec.js" || true
 npx cypress run || true
-npx lighthouse http://localhost:8888/editor.html --output json --output-path=./lighthouse.report.json
+npx lighthouse http://localhost:8888/editor.html --output json --output-path=./lighthouse.report.json --chrome-flags="--no-sandbox"
