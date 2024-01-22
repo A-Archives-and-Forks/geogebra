da98bd83b0506f04a175a227a7b4a4c338817c93
laszlogal <latz.gal@gmail.com>
2024-01-22 15:41:56 +0100

    APPS-5363 Matrix load test

--- desktop/src/test/java/org/geogebra/deslktop/gui/util/SequenceLoadTest.java
+++ desktop/src/test/java/org/geogebra/deslktop/gui/util/SequenceLoadTest.java
@@ -1,5 +1,7 @@
 package org.geogebra.deslktop.gui.util;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.geogebra.common.BaseUnitTest;
 import org.geogebra.common.euclidian.DrawEquation;
 import org.geogebra.common.jre.headless.AppCommon;
@@ -14,15 +16,18 @@ import org.junit.Test;
 
 public class SequenceLoadTest extends BaseUnitTest {
 
+	public static final String M2 =
+			"{{2, 0, 0, 0}, {2, 1, 0, 0}, {2, 0, 0, 0}, {2, 1, 0, 0}, {8, 1, 0, 0}, {8, 4, 1, 0},"
+					+ " {8, 4, 0, 0}}";
 	static AppCommon app;
 	private UndoRedoTester undoRedoTester;
 
 	public static final String sequenceBase64 = Base64.encodeToString(
 			UtilD.loadFileIntoByteArray("src/test/resources/sequence-undo.ggb"), false);
 
-	@Before
-	public void setUp() {
-		app = new AppDNoGui(new LocalizationD(3), false) {
+	@Override
+	public AppCommon createAppCommon() {
+		return new AppDNoGui(new LocalizationD(3), false) {
 
 			@Override
 			public DrawEquation getDrawEquation() {
@@ -30,12 +35,27 @@ public class SequenceLoadTest extends BaseUnitTest {
 			}
 
 		};
+	}
 
+	@Before
+	public void setUp() {
+		app = getApp();
 		app.getGgbApi().setBase64(sequenceBase64);
 		undoRedoTester = new UndoRedoTester(app);
 		undoRedoTester.setupUndoRedo();
 	}
 
+	@Test
+	public void testMatrixIsLoaded() {
+		assertThat(app.getKernel().lookupLabel("m2"), hasValue(M2));
+	}
+
+	@Test
+	public void testMatrixFromCommandAfterLoad() {
+		add("Sequence(Sequence(Element(m1, lig, col) - Element(m1, lig, col + 1), col, 1, dimjeu2),"
+				+ " lig, 1, Dimension(jeunonnul))");
+		assertThat(app.getKernel().lookupLabel("m4"), hasValue(M2));
+	}
 
 	@Test
 	public void testUndo() {
