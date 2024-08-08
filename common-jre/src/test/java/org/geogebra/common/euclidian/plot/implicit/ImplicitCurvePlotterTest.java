7c3bb534780258f39ac9c673d5959cd7031475d9
laszlogal <latz.gal@gmail.com>
2024-08-08 10:07:12 +0200

    APPS-5655 split cells recursively

---
+++ common-jre/src/test/java/org/geogebra/common/euclidian/plot/implicit/ImplicitCurvePlotterTest.java
@@ -0,0 +1,31 @@
+package org.geogebra.common.euclidian.plot.implicit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.geogebra.common.BaseUnitTest;
+import org.geogebra.common.euclidian.EuclidianView;
+import org.geogebra.common.euclidian.EuclidianViewBoundsImp;
+import org.geogebra.common.kernel.geos.GeoElement;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImplicitCurvePlotterTest extends BaseUnitTest {
+	private ImplicitCurvePlotter plotter;
+	private EuclidianView view;
+	private EuclidianViewBoundsImp bounds;
+
+	@Before
+	public void setUp() {
+		view = getApp().getActiveEuclidianView();
+		bounds = new EuclidianViewBoundsImp(view);
+	}
+
+	@Test
+	public void testUpdate() {
+		GeoElement curve = add("x^3 - y^3 - 0.6 = 0");
+		plotter = new ImplicitCurvePlotter(curve, bounds);
+		plotter.update();
+		assertEquals(64, plotter.subContentCount());
+	}
+
+}
--- common/src/main/java/org/geogebra/common/euclidian/plot/implicit/ImplicitCurvePlotter.java
+++ common/src/main/java/org/geogebra/common/euclidian/plot/implicit/ImplicitCurvePlotter.java
@@ -1,23 +1,27 @@
 package org.geogebra.common.euclidian.plot.implicit;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.geogebra.common.awt.GGraphics2D;
 import org.geogebra.common.euclidian.plot.interval.EuclidianViewBounds;
 import org.geogebra.common.kernel.arithmetic.BernsteinPolynomial;
 import org.geogebra.common.kernel.arithmetic.BernsteinPolynomialConverter;
-import org.geogebra.common.kernel.implicit.GeoImplicitCurve;
+import org.geogebra.common.kernel.geos.GeoElement;
 
 public class ImplicitCurvePlotter {
-	public static final int MAX_RECURSION = 3;
-	private final BernsteinPolynomial polynomial;
-	private final CurvePlotContext context;
-	private BernsteinPolynomialConverter converter = new BernsteinPolynomialConverter();
+	public static final int MAX_SPLIT_RECURSION = 3;
+	private final List<CurvePlotContext> subContexts = new ArrayList<>();
 
-	public ImplicitCurvePlotter(GeoImplicitCurve curve, EuclidianViewBounds bounds) {
-		polynomial = converter.from(curve, bounds.getXmin(), bounds.getXmax());
-		context = new CurvePlotContext(new CurvePlotBoundingBox(
+	public ImplicitCurvePlotter(GeoElement curve, EuclidianViewBounds bounds) {
+		BernsteinPolynomialConverter converter = new BernsteinPolynomialConverter();
+		BernsteinPolynomial polynomial = converter.from(curve, bounds.getXmin(), bounds.getXmax());
+		CurvePlotContext intitialContext = new CurvePlotContext(new CurvePlotBoundingBox(
 				bounds.getXmin(), bounds.getYmin(),
 				bounds.getXmax(), bounds.getYmax()),
 				polynomial);
+		subContexts.add(intitialContext);
 	}
 
 	public void draw(GGraphics2D g2) {
@@ -29,8 +33,21 @@ public class ImplicitCurvePlotter {
 	}
 
 	public void update() {
-		for (CurvePlotContext ctx: context.split()) {
-			ctx.process();
+		for (int i = 0; i < MAX_SPLIT_RECURSION; i++) {
+			split();
 		}
+	}
+
+	private void split() {
+		List<CurvePlotContext> list = new ArrayList<>();
+		for (CurvePlotContext ctx: subContexts) {
+			Collections.addAll(list, ctx.split());
+		}
+		subContexts.clear();
+		subContexts.addAll(list);
+	}
+
+	public int subContentCount() {
+		return subContexts.size();
 	}
 }
