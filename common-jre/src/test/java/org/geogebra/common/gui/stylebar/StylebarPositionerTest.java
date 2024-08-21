a6aebffdfceb468575eda81b9a666ca2077ac307
Zbynek Konecny <zbynek@geogebra.at>
2024-08-21 11:03:44 +0200

    APPS-4981 Fix warnings

--- common-jre/src/test/java/org/geogebra/common/gui/stylebar/StylebarPositionerTest.java
+++ common-jre/src/test/java/org/geogebra/common/gui/stylebar/StylebarPositionerTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 public class StylebarPositionerTest extends BaseUnitTest {
 
 	StylebarPositioner sp;
+
 	@Before
 	public void setupViewport() {
 		add("ZoomIn(0,0,8,6)");
@@ -24,16 +25,16 @@ public class StylebarPositionerTest extends BaseUnitTest {
 	@Test
 	public void positionForPoint() {
 		getApp().getSelectionManager().addSelectedGeo(add("A=(1,1)"));
-		assertEquals(new GPoint(116,525), sp.getPositionForStyleBar(50, 50));
+		assertEquals(new GPoint(116, 525), sp.getPositionForStyleBar(50, 50));
 		// bigger style bar won't fit below, show above
-		assertEquals(new GPoint(116,375), sp.getPositionForStyleBar(50, 100));
+		assertEquals(new GPoint(116, 375), sp.getPositionForStyleBar(50, 100));
 	}
 
 	@Test
 	public void positionForTwoPoints() {
 		getApp().getSelectionManager().addSelectedGeo(add("A=(1,1)"));
 		getApp().getSelectionManager().addSelectedGeo(add("A=(7,5)"));
-		assertEquals(new GPoint(716,125), sp.getPositionForStyleBar(50, 50));
+		assertEquals(new GPoint(716, 125), sp.getPositionForStyleBar(50, 50));
 	}
 
 	@Test
@@ -41,11 +42,11 @@ public class StylebarPositionerTest extends BaseUnitTest {
 		GeoElement function = add("f:x");
 		pointerDown(200, 400);
 		assertTrue("Should select function", function.isSelected());
-		assertEquals(new GPoint(210,410), sp.getPositionForStyleBar(50, 50));
+		assertEquals(new GPoint(210, 410), sp.getPositionForStyleBar(50, 50));
 		getApp().getSelectionManager().removeAllSelectedGeos();
 		assertThat(sp.getPositionForStyleBar(50, 50), nullValue());
 		getApp().getSelectionManager().addSelectedGeo(function);
-		assertEquals(new GPoint(210,410), sp.getPositionForStyleBar(50, 50));
+		assertEquals(new GPoint(210, 410), sp.getPositionForStyleBar(50, 50));
 	}
 
 	@Test
@@ -53,14 +54,15 @@ public class StylebarPositionerTest extends BaseUnitTest {
 		add("f:x");
 		GeoElement inputBox = add("InputBox()");
 		pointerDown(200, 400);
-		assertEquals(new GPoint(210,410), sp.getPositionForStyleBar(50, 50));
+		assertEquals(new GPoint(210, 410), sp.getPositionForStyleBar(50, 50));
 		pointerDown(50, 50);
 		assertTrue("Should select input box", inputBox.isSelected());
 		assertThat(sp.getPositionForStyleBar(50, 50), nullValue());
 	}
 
 	private void pointerDown(int x, int y) {
-		getApp().getActiveEuclidianView().getEuclidianController().wrapMousePressed(new TestEvent(x, y));
+		getApp().getActiveEuclidianView().getEuclidianController()
+				.wrapMousePressed(new TestEvent(x, y));
 	}
 
 }
