5b5980354d22c32cbf43272088dd3561f1e0cedb
Zoltán Finta <zoltan.finta@geogebra.at>
2024-09-18 13:01:29 +0300

    APPS-5898 Added GeoGebraConstants to SuiteSubApp conversion

--- common/src/main/java/org/geogebra/common/SuiteSubApp.java
+++ common/src/main/java/org/geogebra/common/SuiteSubApp.java
@@ -2,6 +2,8 @@ package org.geogebra.common;
 
 import static org.geogebra.common.GeoGebraConstants.*;
 
+import javax.annotation.Nullable;
+
 public enum SuiteSubApp {
 	GRAPHING(GRAPHING_APPCODE),
 	GEOMETRY(GEOMETRY_APPCODE),
@@ -14,5 +16,17 @@ public enum SuiteSubApp {
 
 	SuiteSubApp(String appCode) {
 		this.appCode = appCode;
+	}
+
+	public static @Nullable SuiteSubApp parse(@Nullable String appCode) {
+		switch (appCode) {
+		case GRAPHING_APPCODE: return GRAPHING;
+		case GEOMETRY_APPCODE: return GEOMETRY;
+		case G3D_APPCODE: return G3D;
+		case CAS_APPCODE: return CAS;
+		case PROBABILITY_APPCODE: return PROBABILITY;
+		case SCIENTIFIC_APPCODE: return SCIENTIFIC;
+		default: return null;
+		}
 	}
 }
