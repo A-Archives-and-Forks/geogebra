bcbc10495261a186895d84a0c353b82c475f6193
csillasolyom-gecse <csilla@geogebra.at>
2024-09-20 09:39:41 +0200

    restore broken changes from conflict resolution

--- web/src/main/java/org/geogebra/web/full/euclidian/quickstylebar/QuickStylebar.java
+++ web/src/main/java/org/geogebra/web/full/euclidian/quickstylebar/QuickStylebar.java
@@ -145,7 +145,7 @@ public class QuickStylebar extends FlowPanel implements EuclidianStyleBar {
 			if (popupMenu.isMenuShown()) {
 				popupMenu.hideMenu();
 			} else {
-				popupMenu.show(this, 0, getOffsetHeight() + POPUP_MENU_DISTANCE);
+				popupMenu.show(contextMenuBtn, 0, getOffsetHeight() + POPUP_MENU_DISTANCE);
 				getApp().registerPopup(popupMenu.getPopupPanel());
 			}
 
