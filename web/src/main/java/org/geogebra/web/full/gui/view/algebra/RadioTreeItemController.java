fc8762975c5e5b2f49779a78aa5ff685ab4e823b
Zbynek Konecny <zbynek@geogebra.at>
2024-10-11 17:18:54 +0200

    APPS-6003 Fix warning

--- web/src/main/java/org/geogebra/web/full/gui/view/algebra/RadioTreeItemController.java
+++ web/src/main/java/org/geogebra/web/full/gui/view/algebra/RadioTreeItemController.java
@@ -673,8 +673,6 @@ public class RadioTreeItemController implements ClickHandler,
 	/**
 	 * @param keepFocus
 	 *            whether focus should stay
-	 * @param createSliders
-	 *            whether to create sliders
 	 */
 	public void onEnter(boolean keepFocus) {
 		// overridden in subclass
