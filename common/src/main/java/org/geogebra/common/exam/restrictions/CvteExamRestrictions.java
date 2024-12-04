d3d655ff0ece078236bf3f59810239c7981759a5
Karl Traunmüller <karl.traunmueller@geogebra.at>
2024-12-04 11:42:58 +0100

    APPS-5926 added new exam feature restriction

--- common/src/main/java/org/geogebra/common/exam/restrictions/CvteExamRestrictions.java
+++ common/src/main/java/org/geogebra/common/exam/restrictions/CvteExamRestrictions.java
@@ -101,7 +101,9 @@ final class CvteExamRestrictions extends ExamRestrictions {
 	}
 
 	private static Set<ExamFeatureRestriction> createFeatureRestrictions() {
-		return Set.of(ExamFeatureRestriction.AUTOMATIC_GRAPH_SELECTION_FOR_FUNCTIONS);
+		return Set.of(
+				ExamFeatureRestriction.AUTOMATIC_GRAPH_SELECTION_FOR_FUNCTIONS,
+				ExamFeatureRestriction.HIDE_CALCULATED_EQUATION);
 	}
 
 	private static Set<CommandFilter> createCommandFilters() {
